d9f6376ea002728b3dfbce849171cc10ef0b1561
Michele <michelebrunasilva@gmail.com>
2023-06-26 20:25:35 -0300

    Adicionando lista para massa de teste

--- main.py
+++ main.py
@@ -76,6 +76,34 @@ def teste_dividir_negativo():
     assert resultado_obtido == resultado_esperado
 
 
+ # lista para uso como massa de teste
+lista_de_valores = [
+    (8, 7, 15),
+    (20, 30, 50),
+    (25, 0, 25),
+    (-5, 12, 7),
+    (6, -3, 3)
+    ]
+
+@pytest.mark.parametrize('numero_a, numero_b, resultado_esperado', lista_de_valores)
+def teste_somar_leitura_de_lista(numero_a, numero_b, resultado_esperado):
+        # O teste é sempre dividido em três partes
+        # 1 - Configura
+        # Utilizamos a lista como massa de teste
+
+
+        # 2 - Executa
+        resultado_obtido = somar(numero_a, numero_b)
+
+        # 3 - Valida
+        assert resultado_esperado == resultado_obtido  # assert é o comando para comparar os dados
+
+
+
+
+
+
+
 # TDD - Teste Driven tedelopment
 #       Desenvolvimento Direcionado por Teste
 #
